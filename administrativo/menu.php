68df35466719500cdcb90278810a3cd01b513b9d
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-07-29 21:19:57 -0500

    Update menu.php

--- administrativo/menu.php
+++ administrativo/menu.php
@@ -12,65 +12,85 @@
 		
 		<ul class="sidebar-menu" data-widget="tree">
 			<li class="header">MENU</li>
-			<li class="treeview active">
+			<li class="treeview">
 				<a href="#">
-					<i class=""></i><span>&ensp;Administración del sistema
-</span>
+					<i class="fa fa-mortar-board"></i><span>&ensp;Alumnos</span>
 					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
 				<ul class="treeview-menu">
-					<li><a href="index.php?tp=1"><i class="fa fa-list-alt"></i> Administración de usuarios </a></li>
-					<li><a href="index.php?tp=4"><i class="fa fa-group"></i> Administración de clientes </a></li>
+					<li><a href="index.php?tp=1"><i class="fa fa-list-alt"></i> Lista </a></li>
+					
+					 <li class="treeview">
+                  <a href="#"><i class="fa fa-user-plus"></i> Nuevo
+                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                  </a>
+                  <ul class="treeview-menu">
+                    <li><a href="carreras.php"><i class="fa fa-circle-o"></i> Carreras</a></li>                   
+                  </ul>
+                </li>
+					<li><a href="index.php?tp=4"><i class="fa fa-group"></i> Exalumnos </a></li>
+					<li><a href="index.php?tp=5"><i class="fa fa-user-circle"></i> Bajas</a></li>
 					  
 				</ul>
 			</li><?php	include("alu_nuevo.php");	?>
 
-			<li class="treeview active">
+			<li class="treeview">
 				<a href="#">
-					<i class="fa fa-drivers-license-o"></i><span>&ensp;Recepción</span>
+					<i class="fa fa-drivers-license-o"></i><span>&ensp;Profesores</span>
 					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
 				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-list-alt"></i> Solicitudes </a></li>
-					<li><a href="#"><i class="fa fa-user-plus"></i> Cotizaciones </a></li>
+					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
+					<li><a href="#"><i class="fa fa-user-plus"></i> Nuevo </a></li>
 				</ul>
 			</li>
 			
-			<li class="treeview active">
+			<li class="treeview">
 				<a href="#">
-					<i class="fa fa-vcard"></i><span>&ensp;Rastreabilidad</span>
+					<i class="fa fa-vcard"></i><span>&ensp;Usuarios</span>
 					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
 				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-vcard-o"></i> Cotizaciones </a></li>
-					<li><a href="#"><i class="fa fa-user-plus"></i> Unidades </a></li>
+					<li><a href="#"><i class="fa fa-vcard-o"></i> Lista </a></li>
+					<li><a href="#"><i class="fa fa-user-plus"></i> Nuevo </a></li>
 				</ul>
 			</li>
 
 
 			<li class="treeview">
 				<a href="#">
-					<i class="fa fa-line-chart"></i><span>&ensp;Calidad</span>
+					<i class="fa fa-line-chart"></i><span>&ensp;Carreras</span>
+					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
+				<ul class="treeview-menu">
+					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
+					<li><a href="#"><i class="fa fa-plus-square"></i> Nuevo </a></li>
+				</ul>
 			</li>
 
 
 			<li class="treeview">
 				<a href="#">
-					<i class="fa fa-th"></i><span>&ensp;Facturación</span>
+					<i class="fa fa-th"></i><span>&ensp;Materias</span>
+					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
+				<ul class="treeview-menu">
+					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
+					<li><a href="#"><i class="fa fa-plus-square"></i> Nuevo </a></li>
+				</ul>
 			</li>
 
 			<li class="treeview">
 				<a href="#">
-					<i class="fa fa-th"></i><span>&ensp;Cobranza</span>
+					<i class="fa fa-calendar"></i><span>&ensp;Horarios</span>
+					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
-			</li>
-
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-calendar"></i><span>&ensp;RH</span>
-				</a>
+				<ul class="treeview-menu">
+					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
+					<li><a href="#"><i class="fa fa-plus-square"></i> Nuevo </a></li>
+				</ul>
 			</li>
 		</ul>
 	</section>

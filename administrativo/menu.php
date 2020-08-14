4ad217f4d89b120eb0a6646cf4c726d261c38efd
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-13 20:54:20 -0500

    Update menu.php

--- administrativo/menu.php
+++ administrativo/menu.php
@@ -20,11 +20,11 @@
 				<ul class="treeview-menu">
 					<li><a href="index.php?tp=1"><i class="fa fa-list-alt"></i> Administraci&oacute;n de Usuarios </a></li>
 					<li class="treeview">
-						<a href="index.php?tp=4"><i class="fa fa-group"></i> Administraci&oacute;n de Clientes </a>
-						<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+						<a href="index.php?tp=4"><i class="fa fa-group"></i> Administraci&oacute;n de Clientes 
+						<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
 						<ul class="treeview-menu">
-							<li><a href="index.php?tp=4"><i class="fa fa-group"></i> Intermediarios </a></li>
-							<li><a href="index.php?tp=4"><i class="fa fa-group"></i> Clientes </a></li>
+							<li><a href="clientes-inter.php"><i class="fa fa-group"></i> Intermediarios </a></li>
+							<li><a href="clientes.php"><i class="fa fa-group"></i> Clientes </a></li>
 						</ul>
 					</li>
 					  
@@ -37,56 +37,25 @@
 					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
 				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
-					<li><a href="#"><i class="fa fa-user-plus"></i> Nuevo </a></li>
+					<li><a href="#"><i class="fa fa-list-alt"></i> Solicitudes </a></li>
+					<li><a href="#"><i class="fa fa-user-plus"></i> Cotizaciones </a></li>
 				</ul>
 			</li>
 			
 			<li class="treeview">
 				<a href="#">
-					<i class="fa fa-vcard"></i><span>&ensp;Usuarios</span>
+					<i class="fa fa-vcard"></i><span> Rastreabilidad</span>
 					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 				</a>
 				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-vcard-o"></i> Lista </a></li>
-					<li><a href="#"><i class="fa fa-user-plus"></i> Nuevo </a></li>
+					<li><a href="#"><i class="fa fa-vcard-o"></i> Procesos </a></li>
+					<li><a href="#"><i class="fa fa-user-plus"></i> Unidades </a></li>
 				</ul>
 			</li>
-
-
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-line-chart"></i><span>&ensp;Carreras</span>
-					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
-					<li><a href="#"><i class="fa fa-plus-square"></i> Nuevo </a></li>
-				</ul>
-			</li>
-
-
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-th"></i><span>&ensp;Materias</span>
-					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
-					<li><a href="#"><i class="fa fa-plus-square"></i> Nuevo </a></li>
-				</ul>
-			</li>
-
-			<li class="treeview">
-				<a href="#">
-					<i class="fa fa-calendar"></i><span>&ensp;Horarios</span>
-					<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="#"><i class="fa fa-list-alt"></i> Lista </a></li>
-					<li><a href="#"><i class="fa fa-plus-square"></i> Nuevo </a></li>
-				</ul>
-			</li>
+			<li><a href="#"><i class="fa fa-line-chart"></i><span> Calidad</span></a></li>
+			<li><a href="#"><i class="fa fa-line-chart"></i><span> Facturaci&oacute;n</span></a></li>
+			<li><a href="#"><i class="fa fa-line-chart"></i><span> Cobranza</span></a></li>
+			<li><a href="#"><i class="fa fa-line-chart"></i><span> RH</span></a></li>
 		</ul>
 	</section>
 </aside>

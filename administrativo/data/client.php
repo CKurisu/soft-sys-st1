0841c5b9f601147960e89cd450ef3c06d7510611
katerin1202 <64759909+katerin1202@users.noreply.github.com>
2020-08-13 22:37:43 -0500

    Update client.php

--- administrativo/data/client.php
+++ administrativo/data/client.php
@@ -34,11 +34,8 @@
 							<label>Tipo Cliente</label>
 							<select class="form-control" name="semestre" id="semestre">
 								<option>--</option>
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>Clientes Siralab</option>
+								<option>Cliente</option>								
 							</select>
 						</div>
 					</div>

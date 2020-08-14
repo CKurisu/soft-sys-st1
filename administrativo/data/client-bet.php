7d8fe079088aeb96b88b7c68c027bbfdfdffd816
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-13 21:16:57 -0500

    Update client-bet.php

--- administrativo/data/client-bet.php
+++ administrativo/data/client-bet.php
@@ -44,6 +44,57 @@
 	</div>
 </div>
 
+ <div class="modal fade" id="ModalDetalle" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+ <div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel"> Detalle de Registro</h4>
+			</div>
+			<div class="modal-body">
+				<div class="row">
+					<div class="form-group col-md-3">
+						<label>ID Cliente</label>
+						<input type="text" name="matricula" id="matricula" class="form-control" placeholder="00001243" readonly/>
+					</div>
+					<div class="form-group col-md-3">
+						<label>Status</label>
+						<div><span class="label label-success">Activo</span></div><br />
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col-md-3">
+						<label>Nombre Cliente</label>
+						<input type="text" name="appaterno" id="appaterno" class="form-control" placeholder="Nombre Cliente" />
+					</div>
+					<div class="form-group col-md-3">
+						<label>Intermediario</label>
+						<input type="text" name="apmaterno" id="apmaterno" class="form-control" placeholder="Intermediario" />
+					</div>
+					<div class="form-group col-md-3">
+						<label>Estado</label>
+						<input type="text" name="correo" id="correo" class="form-control" placeholder="Estado" />
+					</div> 
+					<div class="form-group col-md-3">
+						<label>Tipo Cliente</label>
+						<select class="form-control" name="semestre" id="semestre">
+							<option>--</option>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="submit" class="btn btn-primary" id="guardarnuevo">Dar de Alta</button>        
+			</div>
+		</div>
+	</div>
+</div>	
+
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr class="tab_tr">
@@ -66,8 +117,10 @@
        		<td class="tab_centrar"></td>
        		<td class="tab_centrar"></td>
        		<td class="tab_centrar"></td>
+			<td class="tab_centrar"><a data-toggle="modal" data-target="#ModalDetalle" class="btn azul"><i class="fas fa-info"></i> Detalle</a></td>
 			<td class="tab_centrar"><a data-toggle="modal" data-target="#ModalModificar" class="btn cafe"><i class="fas fa-edit"></i> Modificar</a></td>
 			<td class="tab_centrar"><a href="#" class="btn rojo"><i class="fas fa-times-circle"></i> Eliminar</a></td>
+			
        	</tr>
 		<?php
 			/*$x=0;

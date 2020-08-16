4f33ccc6a43a29b8a51f7d323a82632c12f4e37d
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-16 15:27:01 -0500

    Update client-bet.php

--- administrativo/data/client-bet.php
+++ administrativo/data/client-bet.php
@@ -5,7 +5,7 @@
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h3 class="modal-title" id="myModalLabel"> Datos Cliente Intermedio</h3>
 			</div>
-				<form id="registerForm" name="formRegister" novalidate action="insert.php" method="POST">
+				<form id="yui11011001" name="yui11011001" novalidate>
 				<div class="modal-body">
 					<div class="row">
 						<div class="form-group col-md-3">
@@ -117,7 +117,7 @@
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h3 class="modal-title" id="myModalLabel"> Datos Cliente Intermedio</h3>
 			</div>
-				<form id="registerForm" name="formRegister" novalidate method="POST">
+				<form id="yui11011002" name="yui11011002" novalidate method="POST">
 				<div class="modal-body">
 					<div class="row">
 						<div class="form-group col-md-3">

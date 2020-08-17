f8633a4ae889cdd7826308dc2c5fdb5e072c3bf9
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-16 21:46:48 -0500

    Create htmsg.cls.php

---
+++ clss/htmsg.cls.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ *  Develop Site
+ *  Developed with Bootstrap.
+ * 
+ *  @version     v1.0.1, built on 2020-08-16 07:38:43 PM
+ *  @author      SoftSystem, LTD.
+ *  @copyright   (c) 2013 - SoftSystem, LTD. All rights reserved.
+ *  @license     Licensed under the MIT license
+ *               Commercial: http://creativecommons.org/license/
+ *               Non-commercial: http://creativecommons.org/licenses/by-nc-nd/3.0/
+ */
+class htmsg {
+    public function success($msg) {return '<div class="alert alert-success text-center"><button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button><strong>Exito!. </strong>'.$msg.'.</div>';}
+    public function danger($msg){return '<div class="alert alert-danger text-center"><button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button><strong>Peligro!. </strong>'.$msg.'.</div>';}
+    public function dangerEx(){return '<div class="alert alert-danger text-center"><button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button><strong>Error!. </strong>Ocurrio un error al realizar la operacion, intente de nuevo mas tarde, estamos trabajando para solucionarlo.</div>';}
+    public function info($msg) {return '<div class="alert alert-info text-center"><button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button><strong>Aviso!. </strong>'.$msg.'.</div>';}
+    public function warning($msg) {return '<div class="alert alert-warning text-center"><button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button><strong>Advertencia!. </strong>'.$msg.'.</div>';}
+}

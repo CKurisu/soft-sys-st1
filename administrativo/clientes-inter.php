0d8f45b1162fc0807da6fe31f8f0e7893ef4d19d
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-18 21:14:11 -0500

    Update clientes-inter.php

--- administrativo/clientes-inter.php
+++ administrativo/clientes-inter.php
@@ -1,15 +1,4 @@
 <?php
-/* 
- *  Develop Site
- *  Developed with Bootstrap.
- * 
- *  @version     v1.0.1, built on 2020-08-16 06:40:40 PM
- *  @author      SoftSystem, LTD.
- *  @copyright   (c) 2013 - SoftSystem, LTD. All rights reserved.
- *  @license     Licensed under the MIT license
- *               Commercial: http://creativecommons.org/license/
- *               Non-commercial: http://creativecommons.org/licenses/by-nc-nd/3.0/
- */
 session_start();
 include '../clss/htmsg.cls.php';
 require '../cnx/cnx.php';

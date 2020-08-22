171e35ba9d9d924ced2ac7d50c82641877618e59
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-21 23:22:18 -0500

    Update clntdt.php

--- administrativo/frmsbmt/clntdt.php
+++ administrativo/frmsbmt/clntdt.php
@@ -3,9 +3,6 @@ session_start();
 if(empty($_POST['x1'])  ||
     empty($_POST['x2']) ||
     empty($_POST['x3']) ||
-    empty($_POST['x4']) ||
-    empty($_POST['x5']) ||
-    empty($_POST['x6']) ||
     empty($_POST['x7']) ||
     empty($_POST['x8']) ||
     empty($_POST['x9']) ||
@@ -35,11 +32,8 @@ try {
         $nomusr =$idusr->fetchColumn();
     }
     $lab=$_POST['x1'];
-    $nomcli=$_POST['x2'];
+    $rznscl=$_POST['x2'];
     $idint=$_POST['x3'];
-    $nom=$_POST['x4'];
-    $app=$_POST['x5'];
-    $apm=$_POST['x6'];
     $rfc=$_POST['x7'];
     $str=$_POST['x8'];
     $numex=$_POST['x9'];
@@ -59,37 +53,51 @@ try {
     $resultset=$statement->rowCount();
     $cnxPDO->beginTransaction();
     if($resultset){
-        $trclirg=$cnxPDO->prepare("UPDATE {$tbl_cli} SET CliLab=:lab,`CliNomEm`=:nomcli,`CliNom`=:nom,`CliApp`=:app,`CliApm`=:apm,`IdInt`=:idint,`CliNomMod`=:usr,`CliDateMod`=:dt WHERE IdCliente=:id;");
-        $trclirg->bindParam(':lab', $lab, PDO::PARAM_STR);
-        $trclirg->bindParam(':nomcli', $nomcli, PDO::PARAM_STR);
-        $trclirg->bindParam(':nom', $nom, PDO::PARAM_STR);
-        $trclirg->bindParam(':app', $app, PDO::PARAM_STR);
-        $trclirg->bindParam(':apm', $apm, PDO::PARAM_STR);
-        $trclirg->bindParam(':idint', $idint, PDO::PARAM_INT);
-        $trclirg->bindParam(':usr', $nomusr, PDO::PARAM_STR);
-        $trclirg->bindParam(':dt', $dt, PDO::PARAM_STR);
-        $trclirg->bindParam(':id', $code, PDO::PARAM_INT);
-        $trclirg->execute();
-        $trclislrg=$cnxPDO->prepare("UPDATE {$tbl_clisl} SET `RFC`=:rfc,`AddInf`=:add,`Calle`=:str,`NumExt`=:numex,`NumInt`=:numin,`Col`=:col,`CP`=:cp,`Loc`=:loc,`Mun`=:mun,`Cd`=:cd,`Edo`=:edo WHERE IdCliente=:id;");
-        $trclislrg->bindParam(':rfc', $rfc, PDO::PARAM_STR);
-        $trclislrg->bindParam(':add', $add, PDO::PARAM_STR);
-        $trclislrg->bindParam(':str', $str, PDO::PARAM_STR);
-        $trclislrg->bindParam(':numex', $numex, PDO::PARAM_STR);
-        $trclislrg->bindParam(':numin', $numin, PDO::PARAM_STR);
-        $trclislrg->bindParam(':col', $col, PDO::PARAM_STR);
-        $trclislrg->bindParam(':cp', $cp, PDO::PARAM_INT);
-        $trclislrg->bindParam(':loc', $loc, PDO::PARAM_STR);
-        $trclislrg->bindParam(':mun', $mun, PDO::PARAM_STR);
-        $trclislrg->bindParam(':cd', $cd, PDO::PARAM_STR);
-        $trclislrg->bindParam(':edo', $edo, PDO::PARAM_STR);
-        $trclislrg->bindParam(':id', $code, PDO::PARAM_INT);
-        $trclislrg->execute();
-        $cnxPDO->commit();
-        $message=$html->success('Datos actualizados correctamente');$fail=0;
+        $stcli=$cnxPDO->prepare("SELECT * FROM {$tbl_cli} WHERE CliLab=:lab OR RznScl=:rzn LIMIT 1;");
+        $stcli->bindParam(':lab', $lab, PDO::PARAM_STR);
+        $stcli->bindParam(':rzn', $rznscl, PDO::PARAM_STR);
+        $stcli->execute();
+        $rowcli=$stcli->rowCount();
+        $stclisl=$cnxPDO->prepare("SELECT * FROM {$tbl_clisl} WHERE RFC=:rfc AND Calle=:str AND NumExt=:numex AND NumInt=:numin AND Col=:col OR CP=:cp OR Loc=:loc LIMIT 1;");
+        $stclisl->bindParam(':rfc', $rfc, pdo::PARAM_STR);
+        $stclisl->bindParam(':str', $str, pdo::PARAM_STR);
+        $stclisl->bindParam(':numex', $numex, pdo::PARAM_STR);
+        $stclisl->bindParam(':numin', $numin, pdo::PARAM_STR);
+        $stclisl->bindParam(':col', $col, pdo::PARAM_STR);
+        $stclisl->bindParam(':cp', $cp, pdo::PARAM_STR);
+        $stclisl->bindParam(':loc', $loc, pdo::PARAM_STR);
+        $stclisl->execute();
+        $rowclisl=$stclisl->rowCount();
+        if((!($rowcli))&&(!($rowclisl))){
+            $trclirg=$cnxPDO->prepare("UPDATE {$tbl_cli} SET CliLab=:lab,RznScl=:rzn,IdInt=:idint,CliNomMod=:usr,CliDateMod=:dt WHERE IdCliente=:id;");
+            $trclirg->bindParam(':lab', $lab, PDO::PARAM_STR);
+            $trclirg->bindParam(':rzn', $rznscl, PDO::PARAM_STR);
+            $trclirg->bindParam(':idint', $idint, PDO::PARAM_INT);
+            $trclirg->bindParam(':usr', $nomusr, PDO::PARAM_STR);
+            $trclirg->bindParam(':dt', $dt, PDO::PARAM_STR);
+            $trclirg->bindParam(':id', $code, PDO::PARAM_INT);
+            $trclirg->execute();
+            $trclislrg=$cnxPDO->prepare("UPDATE {$tbl_clisl} SET RFC=:rfc,AddInf=:add,Calle=:str,NumExt=:numex,NumInt=:numin,Col=:col,CP=:cp,Loc=:loc,Mun=:mun,Cd=:cd,Edo=:edo WHERE IdCliente=:id;");
+            $trclislrg->bindParam(':rfc', $rfc, PDO::PARAM_STR);
+            $trclislrg->bindParam(':add', $add, PDO::PARAM_STR);
+            $trclislrg->bindParam(':str', $str, PDO::PARAM_STR);
+            $trclislrg->bindParam(':numex', $numex, PDO::PARAM_STR);
+            $trclislrg->bindParam(':numin', $numin, PDO::PARAM_STR);
+            $trclislrg->bindParam(':col', $col, PDO::PARAM_STR);
+            $trclislrg->bindParam(':cp', $cp, PDO::PARAM_INT);
+            $trclislrg->bindParam(':loc', $loc, PDO::PARAM_STR);
+            $trclislrg->bindParam(':mun', $mun, PDO::PARAM_STR);
+            $trclislrg->bindParam(':cd', $cd, PDO::PARAM_STR);
+            $trclislrg->bindParam(':edo', $edo, PDO::PARAM_STR);
+            $trclislrg->bindParam(':id', $code, PDO::PARAM_INT);
+            $trclislrg->execute();
+            $cnxPDO->commit();
+            $message=$html->success('Datos actualizados correctamente');$fail=0;
+        }else{$message=$html->info('Datos registrados previamente');$fail=1;}
     }else{$message=$html->info('Informacion incorrecta');$fail=1;}
 } catch (Exception $e) {
     $cnxPDO->rollBack();
-    $message=$html->dangerEx();$fail=1;
+    $message=$html->danger($e->getMessage());$fail=1;
 }
 closeCnxP($cnxPDO);
 $returnArray=array("message"=>$message, "fail" => $fail);

c89e9b3d3f65dff5ae55c27870aeb9de862f1d28
katerin1202 <64759909+katerin1202@users.noreply.github.com>
2020-08-21 22:14:57 -0500

    Update siralab.php

--- administrativo/siralab.php
+++ administrativo/siralab.php
@@ -59,7 +59,7 @@
 							switch($_SESSION['tp'])
 							{
 								case 1:
-									echo 'Lista de Clientes';
+									echo 'Reporte Siralab';
 								break;
 								case 2:
 									echo 'Nuevo Alumno';

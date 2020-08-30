94ad3967a2f8c549bd3d3cde978e2d548f210a30
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-30 11:09:51 -0500

    Update clntdlt.php

--- administrativo/frmsbmt/clntdlt.php
+++ administrativo/frmsbmt/clntdlt.php
@@ -13,21 +13,21 @@ try {
     $cnxPDO= initCnx();
     $id=$_POST['v'];
     $cnxPDO->beginTransaction();
-    $stmt=$cnxPDO->prepare("SELECT * FROM {$tbl_clisl} WHERE IdCliente=:id;");
+    $stmt=$cnxPDO->prepare("SELECT * FROM {$tbl_dtcli} WHERE IdCliente=:id;");
     $stmt->bindParam(":id", $id, PDO::PARAM_INT);
     $stmt->execute();
     $result=$stmt->rowCount();
-    $stmtsl=$cnxPDO->prepare("SELECT * FROM {$tbl_cli} WHERE IdCliente=:id;");
-    $stmtsl->bindParam(":id", $id, PDO::PARAM_INT);
-    $stmtsl->execute();
-    $resul=$stmtsl->rowCount();
+    $stmtdt=$cnxPDO->prepare("SELECT * FROM {$tbl_cli} WHERE IdCliente=:id;");
+    $stmtdt->bindParam(":id", $id, PDO::PARAM_INT);
+    $stmtdt->execute();
+    $resul=$stmtdt->rowCount();
     if(($result>0)&&($resul>0)){
-        $stdlcli=$cnxPDO->prepare("DELETE FROM {$tbl_clisl} WHERE IdCliente=:id;");
+        $stdlcli=$cnxPDO->prepare("DELETE FROM {$tbl_dtcli} WHERE IdCliente=:id;");
         $stdlcli->bindParam(':id', $id, PDO::PARAM_INT);
         $stdlcli->execute();
-        $stdlclisl=$cnxPDO->prepare("DELETE FROM {$tbl_cli} WHERE IdCliente=:id;");
-        $stdlclisl->bindParam(':id', $id, PDO::PARAM_INT);
-        $stdlclisl->execute();
+        $stdlclidt=$cnxPDO->prepare("DELETE FROM {$tbl_cli} WHERE IdCliente=:id;");
+        $stdlclidt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stdlclidt->execute();
         $cnxPDO->commit();
         $message=$html->success('Eliminado con exito');$fail=0;
     }else{$message=$html->danger('El usuario no existe');$fail=1;}

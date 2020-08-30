6770a053c82a597863c7b7779a9a68d5983d09e0
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-30 11:09:10 -0500

    Update clientes-informe.php

--- administrativo/clientes-informe.php
+++ administrativo/clientes-informe.php
@@ -47,41 +47,41 @@ session_start();
 			
 				<section class="content-header">
 					<center class="titulo">
-						<?php
-							switch($_SESSION['tp'])
-							{
-								case 1:
-									echo 'Cliente informe';
-								break;
-								case 2:
-									echo 'Nuevo Alumno';
-								break;
-								case 3:
-									echo 'Lista de Alumnos';
-								break;
-							}
-						?>
+                                            Cliente informe
 					</center>
 				</section>
 					
 				<!----------     TITULO     ---------->
 				<section class="content">
-					<?php
-						switch($_SESSION['tp'])
-						{
-							case 1:
-								include("data/clients-report.php");
-							break;
-							case 2:
-								include("alu_nuevo.php");
-								include("alu_nuevo.php");
-							break;
-							case 3:
-								include("alu_nuevog.php");
-								include("alu_lista.php");
-							break;
-						}
-					?>
+					<div class="modal-body">
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr class="tab_tr">
+                    <th scope="col" class="tab_centrar">Estatus</th>
+                    <th scope="col" class="tab_centrar">RFC</th>
+                    <th scope="col" class="tab_centrar">Direccion</th>
+                    <th scope="col" class="tab_centrar">Direccion Informe / Cotizacion</th>
+                    <th scope="col" class="tab_centrar">Puntos De Muestreo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="tab_td">
+                    <th scope="row" class="tab_centrar"></th>
+                    <td class="tab_centrar"><span class="label label-success">Activo</span</td>
+                    <td class="tab_centrar"></td>
+                    <td class="tab_centrar"></td>
+                    <td class="tab_centrar"></td>
+                    <td class="tab_centrar"></td>
+                </tr>
+        </table>
+    </div>
+</div>
+<div class="modal-footer">
+        <button type="submit" class="btn btn-success" id="guardarnuevo">Guardar</button>
+        <button type="submit" class="btn btn-primary" id="guardarnuevo">Modificar</button>
+        <button type="submit" class="btn btn-warning" id="guardarnuevo">Eliminar</button>
+</div>
 				</section>
 			</div>
 			

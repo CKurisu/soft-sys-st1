cbff460bbd5b0c39983d117e9b13e494b7c84f12
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-08-30 11:10:13 -0500

    Update head_menu_js.php

--- head_menu_js.php
+++ head_menu_js.php
@@ -2,25 +2,16 @@
 <script>window.jQuery || document.write('<script src="../js/jquery.js"><\/script>')</script>
 <script src="../js/menu/bootstrap.min.js"></script>
 <script src="../js/menu/temas.min.js"></script>
+<script src="../js/bootstrap-datepicker.min.js"></script>
+<script src="../locales/bootstrap-datepicker.es.min.js"></script>
 <script type="text/javascript">
     $(function (){
-    $(document).on('click','#edicionRegistros',function(e){
-        e.preventDefault();
-        var id=$(this).data('id');
-        $.ajax({
-            url: "./php/preedit.php",
-            type: 'POST',
-            data: {
-                i:id
-            },cache: false,
-            success: function (v) {
-                $('#resultedit').html(v);
-            },error: function () {
-                $('#resultedit').html('Erroe en la operacion');
-            }            
-        });
+    $('.input-daterange').datepicker({
+        autoclose: true,
+        todayHighlight: true
     });
     });
 </script>
 <script src="../js/sweetalert.min.js"></script>
 <script src="../js/jQueryForms.min.js"></script>
+<script src="../js/fildata.min.js"></script>

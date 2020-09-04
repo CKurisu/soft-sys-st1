84d9d7d501fed49b694f2073103cb61dbae467b9
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-03 20:04:54 -0500

    Update vls.php

--- cnx/vls.php
+++ cnx/vls.php
@@ -10,7 +10,7 @@
  *               Commercial: http://creativecommons.org/license/
  *               Non-commercial: http://creativecommons.org/licenses/by-nc-nd/3.0/
  */
-//define conection constants
+//LOCAL SERVER CONECTION VARIABLES
 define("SERVER", "localhost");
 define("USER", "root");
 define("PASS_USER", "");
@@ -19,7 +19,18 @@ define("PASS_USER", "");
 //define("U_PASS", "");
 define("DB_NAME", "corpo133_sii");
 
+//REMOTE SERVER CONECTION VARIABLES
+//define("SERVER", "localhost");
+//define("USER", "softsys_admnsii");
+//define("PASS_USER", "otDr4sG8R){N");
+//define("PORT", "3306");
+//define("SOCKET", "TCP");
+//define("U_PASS", "");
+//define("DB_NAME", "softsys_corpo133_sii");
+
 $tbl_prefix = ""; //Prefix for all database tables
 $tbl_clinter = $tbl_prefix."intermediarios";
 $tbl_cli = $tbl_prefix."clientes";
 $tbl_dtcli = $tbl_prefix."detcli";
+$tbl_user = $tbl_prefix."usuario";
+$tbl_users = $tbl_prefix."usuarios";

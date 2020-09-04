cee9963da3cea6e4850df85f399f86391a10c91a
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-03 20:41:19 -0500

    Update head_menu_css.php

--- head_menu_css.php
+++ head_menu_css.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="../css/bootstrap.min.css" />
 <link rel="stylesheet" href="../css/bootstrap-datepicker3.min.css"/>
+<link rel="stylesheet" href="../css/bootstrapValidator.min.css"/>
 <link rel="stylesheet" href="../css/menu/all.css" />
 <link rel="stylesheet" href="../css/menu/font-awesome.min" />
 <link rel="stylesheet" href="../css/menu/temas.min.css" />

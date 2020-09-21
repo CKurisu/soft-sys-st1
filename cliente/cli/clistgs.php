22b3c72c797c9100e6c0f75807562baa4ca17448
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:13:22 -0500

    Create clistgs.php

---
+++ cliente/cli/clistgs.php
@@ -0,0 +1,11 @@
+<div id="ModalModificar" class="content-wrapper hidden">
+    <div class="content-header">
+        <button type="button" class="close-btn-section close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="btn-to-back btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
+        <div class="titulo">Modificaci&oacute;n de Registro</div>
+    </div>
+    <div id="sttgs-u"></div>
+    <div class="modal-footer">
+        <button type="button" class="btn-to-back btn btn-warning"><span><i class="fas fa-times"></i> Cancelar</span></button>
+    </div>
+</div>

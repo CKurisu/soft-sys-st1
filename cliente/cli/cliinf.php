75c872772b12a45abdbf3a02175a5dd23d23b98f
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:13:14 -0500

    Create cliinf.php

---
+++ cliente/cli/cliinf.php
@@ -0,0 +1,13 @@
+<div id="ModalDetalle" class="content-wrapper hidden">
+    <section class="content-header">
+        <button type="button" class="close-btn-section close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="btn-to-back btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
+        <section class="titulo">Detalle de Registro</section>
+    </section>
+    <section class="content">
+        <div id="info-u"></div>
+    </section>
+    <section class="modal-footer">
+        <button type="button" class="btn-to-back btn btn-success"><span><i class="fas fa-check"></i> Aceptar</span></button>
+    </section>
+</div>

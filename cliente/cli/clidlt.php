94b9066cb7538acdc4750790d6598b2d9d792849
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:13:09 -0500

    Create clidlt.php

---
+++ cliente/cli/clidlt.php
@@ -0,0 +1,15 @@
+<div class="content-wrapper hidden " id="ModalEliminar">
+    <div class="modal-header">
+        <button type="button" class="close-btn-section close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="btn-to-back btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
+        <div class="titulo"> Eliminaci&oacute;n de Registro</div>
+    </div>
+    <div class="modal-body">
+        <div id="messagefrmdlcli"></div>
+        <div class="center-block text-center">Estas seguro de querer eliminar este registro?</div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn-dlst2-u btn btn-danger" data-id=""><i class="fas fa-times-circle"></i> Eliminar</button>
+        <button type="button" class="btn-to-back btn btn-warning"><span><i class="fas fa-times"></i> Cancelar</span></button>   
+    </div>
+</div>

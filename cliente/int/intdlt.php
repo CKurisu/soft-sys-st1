30b3b807307420598b9d7450a4ead9b71fa79815
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:15:55 -0500

    Create intdlt.php

---
+++ cliente/int/intdlt.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="ModalEliminar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"> Eliminaci&oacute;n de Registro</h4>
+            </div>
+            <div class="modal-body">
+                <div id="messagefrmdl"></div>
+                <div class="center-block text-center">Estas seguro de querer eliminar este registro?</div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn-dlst2-uint btn btn-danger" data-id=""><i class="fas fa-times-circle"></i> Eliminar</button>
+                <button type="button" class="btn btn-info" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"> Cerrar</span></button>   
+            </div>
+        </div>
+    </div>
+</div>

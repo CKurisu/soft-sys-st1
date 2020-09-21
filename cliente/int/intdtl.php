54394e79c4268e08d2fe488a8e35e3e7e2d49308
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:16:00 -0500

    Create intdtl.php

---
+++ cliente/int/intdtl.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="ModalDetalle" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"> Detalle de Registro</h4>
+            </div>
+            <div class="modal-body">
+                <div id="info-uint"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"> Cerrar</span></button>   
+            </div>
+        </div>
+    </div>
+</div>

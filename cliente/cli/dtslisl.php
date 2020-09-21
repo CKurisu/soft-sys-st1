484d475d3d72b64791b12c2d76339a638db94d1c
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:13:30 -0500

    Create dtslisl.php

---
+++ cliente/cli/dtslisl.php
@@ -0,0 +1,15 @@
+<div class="content-wrapper hidden" id="DetalleCli2">
+    <div class="modal-header">
+        <button type="button" class="close-btn-section-div close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="btn-to-back-div btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
+        <div class="titulo"> Informaci&oacute;n de Cliente SirLab</div>
+    </div>
+    <div class="modal-body">
+        <div id="info-dtls-u"></div>
+    </div>
+    <div class="modal-footer">
+        <a href="reportes-siralab.php"><button class="btn azul"><i class="fas fa-info"></i> Reporte SirLab</button></a>
+        <button data-toggle="modal" data-target="#ReporteSirLab" class="btn-rpt-slb btn azul" data-id=""><i class="fas fa-info"></i> Reporte SirLab Modal</button>
+        <button type="button" class="btn-to-back-div btn btn-warning"><span><i class="fas fa-times"></i> Cancelar</span></button>
+    </div>
+</div>

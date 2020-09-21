18a83fb1f664950f37ca8fe6ba4db30269a1ebbb
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-21 00:13:26 -0500

    Create dtcli.php

---
+++ cliente/cli/dtcli.php
@@ -0,0 +1,15 @@
+<div class="content-wrapper hidden" id="DetalleCli">
+    <div class="modal-header">
+        <button type="button" class="close-btn-section-div close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="btn-to-back-div btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
+        <div class="titulo"> Informaci&oacute;n de Cliente</div>
+    </div>
+    <div class="modal-body">
+        <div id="info-dtl-u"></div>
+    </div>
+    <div class="modal-footer">
+        <a href="clientes-informe.php"><button class="btn azul"><i class="fas fa-info"></i> Reporte de Cliente</button></a>
+        <button data-toggle="modal" data-target="#ReporteCliente" class="btn-rpt-cli btn azul" data-id=""><i class="fas fa-info"></i> Reporte Cliente Modal</button>
+        <button type="button" class="btn-to-back-div btn btn-warning"><span><i class="fas fa-times"></i> Cancelar</span></button>
+    </div>
+</div>

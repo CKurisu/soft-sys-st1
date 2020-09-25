471b061cb5ba2239ec70314913e8ba4b5a0dac1c
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-25 15:09:54 -0500

    Update head_menu_js.php

--- head_menu_js.php
+++ head_menu_js.php
@@ -1,4 +1,4 @@
-<script src="../js/jquery.min.js"></script>
+<script src="./js/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="../js/jquery.js"><\/script>')</script>
 <script src="../js/menu/bootstrap.min.js"></script>
 <script src="../js/bootstrapValidator.min.js"></script>

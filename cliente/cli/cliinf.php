4c7998e50fc12f4ce2ac2af645be82f10bb98cda
softsystem <34039637+soft-sys@users.noreply.github.com>
2020-09-29 11:04:19 -0500

    Update cliinf.php

--- cliente/cli/cliinf.php
+++ cliente/cli/cliinf.php
@@ -1,13 +1,13 @@
-<div id="ModalDetalle" class="content-wrapper hidden">
+<div id="ModalDetalle" class="content-wrapper">
     <section class="content-header">
-        <button type="button" class="close-btn-section close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <button type="button" class="btn-to-back btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
-        <section class="titulo">Detalle de Registro</section>
+        <button type="button" class="close-btn-section-page close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="close-btn-section-page btn btn-info"><span><i class="fas fa-angle-double-left"></i> Regresar</span></button>
+        <section class="titulo">Historial de Registro Clientes</section>
     </section>
     <section class="content">
-        <div id="info-u"></div>
+        <div id="info-u" onload="open('./frmsbmt/clntinf.php')"></div>
     </section>
     <section class="modal-footer">
-        <button type="button" class="btn-to-back btn btn-success"><span><i class="fas fa-check"></i> Aceptar</span></button>
+        <button type="button" class="close-btn-section-page btn btn-success"><span><i class="fas fa-check"></i> Aceptar</span></button>
     </section>
 </div>
